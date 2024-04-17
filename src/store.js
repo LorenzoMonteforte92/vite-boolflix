64dcba463d274981f581fa9c06a80e52c1e93827
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-04-17 16:47:51 +0200

    fix: languages

--- src/store.js
+++ src/store.js
@@ -5,3 +5,7 @@ export const store = reactive({
     resultMovies: [],
     resultTvShows: []
 });
+
+
+//stampare la bandiera adeguata per ogni lingua
+//se singleMovie.original_language è uguale a en stampa 
