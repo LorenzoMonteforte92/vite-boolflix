75e2c7450c8b20f6271003febfe97b287cd8a7a2
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-04-17 21:47:01 +0200

    feat: add movies and tv shows cover img

--- src/components/MovieCards.vue
+++ src/components/MovieCards.vue
@@ -19,25 +19,37 @@ import { store } from '../store.js';
 
 <template>
     <div v-for="singleMovie in store.resultMovies" class="card" style="width: calc(100% / 5 - 1rem)">
-        <img src="" class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">{{ singleMovie.title }}</p>
-            <p class="card-text">{{ singleMovie.original_title }}</p>
-            <div v-if="singleMovie.original_language === 'it'" class="language-flag" ><img src="../assets/img/icons8-italy.png" alt=""></div>
-            <div v-else-if="singleMovie.original_language === 'fr'" class="language-flag" ><img src="../assets/img/icons8-france.png" alt=""></div>
-            <div v-else-if="singleMovie.original_language === 'es'" class="language-flag" ><img src="../assets/img/icons8-spain.png" alt=""></div>
-            <div v-else-if="singleMovie.original_language === 'en'" class="language-flag" ><img src="../assets/img/icons8-great-britain.png" alt=""></div>
-            <div v-else-if="singleMovie.original_language === 'pt'" class="language-flag" ><img src="../assets/img/icons8-portugal.png" alt=""></div>
-            <div v-else-if="singleMovie.original_language === 'ja'" class="language-flag" ><img src="../assets/img/icons8-japan.png" alt=""></div>
-            <p v-else class="card-text">{{ singleMovie.original_language }}</p>
-            <p class="card-text">{{ singleMovie.vote_average }}</p>
+        <div class="ms-card-img" >
+            <img v-if="singleMovie.backdrop_path != null" :src="`https://image.tmdb.org/t/p/w500${singleMovie.backdrop_path}`" class="card-img-top" alt="...">
+            <img v-else src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3A0wuWXMSjfCUPDgeVF63_08mB46ulS8Mtf-jxo3GBw&s" alt="">
+        </div>
+        <div class="card-body d-flex flex-column justify-content-between mt-4">
+            <div>
+                <p class="card-text">{{ singleMovie.title }}</p>
+                <p class="card-text">{{ singleMovie.original_title }}</p>
+            </div>
+            <div>
+                <div v-if="singleMovie.original_language === 'it'" class="language-flag m-0" ><img src="../assets/img/icons8-italy.png" alt=""></div>
+                <div v-else-if="singleMovie.original_language === 'fr'" class="language-flag m-0" ><img src="../assets/img/icons8-france.png" alt=""></div>
+                <div v-else-if="singleMovie.original_language === 'es'" class="language-flag m-0" ><img src="../assets/img/icons8-spain.png" alt=""></div>
+                <div v-else-if="singleMovie.original_language === 'en'" class="language-flag m-0" ><img src="../assets/img/icons8-great-britain.png" alt=""></div>
+                <div v-else-if="singleMovie.original_language === 'pt'" class="language-flag m-0" ><img src="../assets/img/icons8-portugal.png" alt=""></div>
+                <div v-else-if="singleMovie.original_language === 'ja'" class="language-flag m-0" ><img src="../assets/img/icons8-japan.png" alt=""></div>
+                <div v-else class="card-text">{{ singleMovie.original_language }}</div>
+                <div class="card-text ps-1 pt-1">{{ singleMovie.vote_average }}</div>
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
     .card{
-        height: 100%;
+
+        img{
+            width: 100%;
+            object-fit: contain;
+            
+        }
     }
     .language-flag{
        border-radius: 50%;
@@ -45,5 +57,9 @@ import { store } from '../store.js';
        img{
         width: 10%;
        }
+    }
+
+    .ms-card-img{
+        height: 45%
     }
 </style>
--- src/components/TvCards.vue
+++ src/components/TvCards.vue
@@ -18,33 +18,48 @@ import { store } from '../store.js';
 </script>
 
 <template>
-    <div v-for="singleTvShow in store.resultTvShows" class="card" style="width: calc(100% / 5 - 1rem)">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">{{ singleTvShow.name }}</p>
-            <p class="card-text">{{ singleTvShow.original_name }}</p>
-            <div v-if="singleTvShow.original_language === 'it'" class="language-flag" ><img src="../assets/img/icons8-italy.png" alt=""></div>
-            <div v-else-if="singleTvShow.original_language === 'fr'" class="language-flag" ><img src="../assets/img/icons8-france.png" alt=""></div>
-            <div v-else-if="singleTvShow.original_language === 'es'" class="language-flag" ><img src="../assets/img/icons8-spain.png" alt=""></div>
-            <div v-else-if="singleTvShow.original_language === 'en'" class="language-flag" ><img src="../assets/img/icons8-great-britain.png" alt=""></div>
-            <div v-else-if="singleTvShow.original_language === 'pt'" class="language-flag" ><img src="../assets/img/icons8-portugal.png" alt=""></div>
-            <div v-else-if="singleTvShow.original_language === 'ja'" class="language-flag" ><img src="../assets/img/icons8-japan.png" alt=""></div>
-            <p v-else class="card-text">{{ singleTvShow.original_language }}</p>
-            <p class="card-text">{{ singleTvShow.vote_average }}</p>
+    <div v-for="singleTvShow in store.resultTvShows"  class="card" style="width: calc(100% / 5 - 1rem)">
+        <div class="ms-card-img" >
+            <img v-if="singleTvShow.backdrop_path != null" :src="`https://image.tmdb.org/t/p/w500${singleTvShow.backdrop_path}`" class="card-img-top" alt="...">
+            <img v-else src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3A0wuWXMSjfCUPDgeVF63_08mB46ulS8Mtf-jxo3GBw&s" alt="">
+        </div>
+        <div class="card-body d-flex flex-column justify-content-between mt-4">
+            <div>
+                <p class="card-text">{{ singleTvShow.name }}</p>
+                <p class="card-text">{{ singleTvShow.original_name }}</p>
+            </div>
+            <div>
+                <div v-if="singleTvShow.original_language === 'it'" class="language-flag m-0" ><img src="../assets/img/icons8-italy.png" alt=""></div>
+                <div v-else-if="singleTvShow.original_language === 'fr'" class="language-flag m-0" ><img src="../assets/img/icons8-france.png" alt=""></div>
+                <div v-else-if="singleTvShow.original_language === 'es'" class="language-flag m-0" ><img src="../assets/img/icons8-spain.png" alt=""></div>
+                <div v-else-if="singleTvShow.original_language === 'en'" class="language-flag m-0" ><img src="../assets/img/icons8-great-britain.png" alt=""></div>
+                <div v-else-if="singleTvShow.original_language === 'pt'" class="language-flag m-0" ><img src="../assets/img/icons8-portugal.png" alt=""></div>
+                <div v-else-if="singleTvShow.original_language === 'ja'" class="language-flag m-0" ><img src="../assets/img/icons8-japan.png" alt=""></div>
+                <div v-else class="card-text">{{ singleTvShow.original_language }}</div>
+                <div class="card-text ps-1 pt-1">{{ singleTvShow.vote_average }}</div>
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
     .card{
-        height: 100%;
-    }
 
+        img{
+            width: 100%;
+            object-fit: contain;
+            
+        }
+    }
     .language-flag{
        border-radius: 50%;
 
        img{
         width: 10%;
        }
+    }
+
+    .ms-card-img{
+        height: 45%
     }
 </style>
