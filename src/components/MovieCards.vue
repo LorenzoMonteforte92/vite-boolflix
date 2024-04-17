d0d54e3fe7768d9738e7079a20c55f77c83f2e8e
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-04-17 16:47:18 +0200

    feat: add flags for original language

--- src/components/MovieCards.vue
+++ src/components/MovieCards.vue
@@ -19,11 +19,17 @@ import { store } from '../store.js';
 
 <template>
     <div v-for="singleMovie in store.resultMovies" class="card" style="width: calc(100% / 5 - 1rem)">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="" class="card-img-top" alt="...">
         <div class="card-body">
             <p class="card-text">{{ singleMovie.title }}</p>
             <p class="card-text">{{ singleMovie.original_title }}</p>
-            <p class="card-text">{{ singleMovie.original_language }}</p>
+            <div v-if="singleMovie.original_language === 'it'" class="language-flag" ><img src="../assets/img/icons8-italy.png" alt=""></div>
+            <div v-else-if="singleMovie.original_language === 'fr'" class="language-flag" ><img src="../assets/img/icons8-france.png" alt=""></div>
+            <div v-else-if="singleMovie.original_language === 'es'" class="language-flag" ><img src="../assets/img/icons8-spain.png" alt=""></div>
+            <div v-else-if="singleMovie.original_language === 'en'" class="language-flag" ><img src="../assets/img/icons8-great-britain.png" alt=""></div>
+            <div v-else-if="singleMovie.original_language === 'pt'" class="language-flag" ><img src="../assets/img/icons8-portugal.png" alt=""></div>
+            <div v-else-if="singleMovie.original_language === 'ja'" class="language-flag" ><img src="../assets/img/icons8-japan.png" alt=""></div>
+            <p v-else class="card-text">{{ singleMovie.original_language }}</p>
             <p class="card-text">{{ singleMovie.vote_average }}</p>
         </div>
     </div>
@@ -32,5 +38,12 @@ import { store } from '../store.js';
 <style scoped lang="scss">
     .card{
         height: 100%;
+    }
+    .language-flag{
+       border-radius: 50%;
+
+       img{
+        width: 10%;
+       }
     }
 </style>
--- src/components/TvCards.vue
+++ src/components/TvCards.vue
@@ -23,7 +23,13 @@ import { store } from '../store.js';
         <div class="card-body">
             <p class="card-text">{{ singleTvShow.name }}</p>
             <p class="card-text">{{ singleTvShow.original_name }}</p>
-            <p class="card-text">{{ singleTvShow.original_language }}</p>
+            <div v-if="singleTvShow.original_language === 'it'" class="language-flag" ><img src="../assets/img/icons8-italy.png" alt=""></div>
+            <div v-else-if="singleTvShow.original_language === 'fr'" class="language-flag" ><img src="../assets/img/icons8-france.png" alt=""></div>
+            <div v-else-if="singleTvShow.original_language === 'es'" class="language-flag" ><img src="../assets/img/icons8-spain.png" alt=""></div>
+            <div v-else-if="singleTvShow.original_language === 'en'" class="language-flag" ><img src="../assets/img/icons8-great-britain.png" alt=""></div>
+            <div v-else-if="singleTvShow.original_language === 'pt'" class="language-flag" ><img src="../assets/img/icons8-portugal.png" alt=""></div>
+            <div v-else-if="singleTvShow.original_language === 'ja'" class="language-flag" ><img src="../assets/img/icons8-japan.png" alt=""></div>
+            <p v-else class="card-text">{{ singleTvShow.original_language }}</p>
             <p class="card-text">{{ singleTvShow.vote_average }}</p>
         </div>
     </div>
@@ -32,5 +38,13 @@ import { store } from '../store.js';
 <style scoped lang="scss">
     .card{
         height: 100%;
+    }
+
+    .language-flag{
+       border-radius: 50%;
+
+       img{
+        width: 10%;
+       }
     }
 </style>
